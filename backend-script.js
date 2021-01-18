2c61f9cf95b089b898d6b16f8f6c89d563f42136
KonradG13 <graczykowski.k@gmail.com>
2021-01-18 22:53:20 +0100

    append backend with some functions, like backspace and input

--- backend-script.js
+++ backend-script.js
@@ -1,15 +1,15 @@
 let calculatorData = {
-    display: "",
+    display: 0,
     operator: "",
-    memory: 0
+    memory: 0,
+    displayAsString: "0"
 };
 
-
 let operationsBase = {
-    "+": (memory, display) => {return memory + parseInt(display)},
-    "-": (memory, display) => {return memory - parseInt(display)},
-    "*": (memory, display) => {return memory * parseInt(display)},
-    "/": (memory, display) => {return memory / parseInt(display)}
+    "+": (memory, display) => {return memory + display},
+    "-": (memory, display) => {return memory - display},
+    "*": (memory, display) => {return memory * display},
+    "/": (memory, display) => {return memory / display}
 };
 
 let operate = () => {
@@ -19,39 +19,54 @@ let operate = () => {
 };
 
 let checkForZeroDivision = () => {
-    if(calculatorData.operator === "/" && parseInt(calculatorData.display) === 0) {
+    if(calculatorData.operator === "/" && calculatorData.display == 0) {
         return alert("What are you doing man? Don't divide by 0 on a humble calculator!");
-    }
+    };
 };
 
 let checkForEmptyOperator = () => {
     if(calculatorData.operator === "") {
-        calculatorData.memory = parseInt(calculatorData.display);
-        calculatorData.display = ""
+        calculatorData.memory = calculatorData.display;
+        calculatorData.display = 0;
+        calculatorData.displayAsString = "";
         return;
-    }
+    };
 };
 
 let performCalculation = () => {
-    if(checkIfCalculationViable()){return}
+    if(checkIfCalculationViable()){return};
     calculatorData.memory = operationsBase[calculatorData.operator](calculatorData.memory, calculatorData.display);
     calculatorData.operator = "";
-    calculatorData.display = "";
-
+    calculatorData.display = 0;
+    calculatorData.displayAsString = "";
 };
 
 let checkIfCalculationViable = () => {
-    return ((calculatorData.operator === "/" && calculatorData.display === "0") || (calculatorData.operator === "")) ? true:false;
-}
-
+    return ((calculatorData.operator === "/" && calculatorData.display == 0) || (calculatorData.operator === "")) ? true:false;
+};
 
 let inputtingDisplay = (pressedNumberKey) => {
-    calculatorData
-    calculatorData.display.push(pressedNumberKey);
+    if(displayLengthCheck()){return alert("Too many digits!")};
+    calculatorData.displayAsString = calculatorData.displayAsString.concat(pressedNumberKey);
+    calculatorData.display = convertStringToIntOrFloat(calculatorData.displayAsString)
+};
+
+let convertStringToIntOrFloat = (numberAsString) => {
+    return numberAsString.includes(".") ? parseFloat(numberAsString):parseInt(numberAsString);
+};
+
+let displayLengthCheck = () => {
+    return calculatorData.displayAsString.length() == 10 ? true:false
+};
+
+let backspaceInDisplay = () => {
+    calculatorData.displayAsString = calculatorData.displayAsString.slice(0,-1);
+    calculatorData.display = convertStringToIntOrFloat(calculatorData.displayAsString)
 };
 
 let clearCalculator = () => {
-    calculatorData.display = "";
+    calculatorData.display = 0;
     calculatorData.operator = "";
     calculatorData.memory = 0;
+    calculatorData.displayAsString = "";
 };
